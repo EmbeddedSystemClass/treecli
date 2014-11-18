f5ad3756f0231b086b1ee85e6e905a89b9bb6d4e
Marek Koza <qyx@krtko.org>
2014-11-18 01:10:14 +0100

    Changes in testing tree.

--- test/test1.c
+++ test/test1.c
@@ -45,27 +45,107 @@ const struct treecli_value test1_interface_enabled = {
 	.next = NULL
 };
 
+
+
+/************************* /system/bootloader values **************************/
+
+const struct treecli_value test1_system_bootloader_postbootaction = {
+	.name = "postboot-action",
+	.next = NULL
+};
+
+const struct treecli_value test1_system_bootloader_image = {
+	.name = "image",
+	.next = &test1_system_bootloader_postbootaction
+};
+
+const struct treecli_value test1_system_bootloader_backupimage = {
+	.name = "backup-image",
+	.next = &test1_system_bootloader_image
+};
+
+const struct treecli_value test1_system_bootloader_prebootdelay = {
+	.name = "preboot-delay",
+	.next = &test1_system_bootloader_backupimage
+};
+
+const struct treecli_value test1_system_bootloader_verifyimage = {
+	.name = "verify-image",
+	.next = &test1_system_bootloader_prebootdelay
+};
+
+const struct treecli_value test1_system_bootloader_consolespeed = {
+	.name = "console-speed",
+	.next = &test1_system_bootloader_verifyimage
+};
+
+
+/***************************** /system commands *******************************/
+
 const struct treecli_command test1_system_quit = {
 	.name = "quit",
 	.help = "Exit the whole thing",
 	.exec = test1_system_quit_exec,
+};
+
+/***************************** /system subnodes *******************************/
+
+const struct treecli_node test1_system_bootloader = {
+	.name = "bootloader",
+	.values = &test1_system_bootloader_consolespeed
+};
+
+
+/******************************* /power values ********************************/
+
+
+/****************************** /power commands *******************************/
+
+
+const struct treecli_command test1_power_reboot = {
+	.name = "reboot",
 	.next = NULL
+};
+
+const struct treecli_command test1_power_poweroff = {
+	.name = "poweroff",
+	.next = &test1_power_reboot
+};
+
+/****************************** /power subnodes *******************************/
+
+
+const struct treecli_node test1_power_source = {
+	.name = "source",
+};
+
+
+/******************************* root subnodes ********************************/
+
+const struct treecli_node test1_power = {
+	.name = "inpower",
+	.help = "System power manipulation (poweroff, reboot, power mode)",
+	.commands = &test1_power_poweroff,
+	.subnodes = &test1_power_source
 };
 
 const struct treecli_node test1_interface = {
 	.name = "interface",
 	.values = &test1_interface_enabled,
 	.commands = &test1_interface_print,
-	.next = NULL
+	.next = &test1_power
 };
 
 const struct treecli_node test1_system = {
 	.name = "system",
 	.help = "Various commands for system management",
 	.commands = &test1_system_quit,
+	.subnodes = &test1_system_bootloader,
 	.next = &test1_interface
 };
 
+
+/********************************* root node **********************************/
 const struct treecli_node test1 = {
 	.name = "/",
 	.subnodes = &test1_system
